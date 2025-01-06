c337b3f212e69d3083f2e247733c82ce026cc194
YelyelyeL2023 <okarintimetravels@gmail.com>
2025-01-06 22:10:07 +0500

    Map now refreshes when a new city is entered

--- public/script.js
+++ public/script.js
@@ -1,27 +1,36 @@
-async function weather(){
-  const city = document.getElementById('cityInput').value || 'London';
-    try {
-      // Fetch weather
-      const weatherRes = await fetch(`/weather?city=${city}`);
-      const weatherData = await weatherRes.json();
-      document.getElementById('weather').innerText =
-        JSON.stringify(weatherData, null, 2);
+let map;
 
-      // Initialize map
-      let { coord } = weatherData;
-      const map = L.map('map').setView([coord.lat, coord.lon], 6);
-      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19
-      }).addTo(map);
-      L.marker([coord.lat, coord.lon]).addTo(map);
-    } catch (error) {
-      console.error(error);
+async function weather() {
+  const city = document.getElementById('cityInput').value || 'London';
+  try {
+    let weatherRes = await fetch(`/weather?city=${city}`);
+    let weatherData = await weatherRes.json();
+    document.getElementById('weather').innerText =
+      JSON.stringify(weatherData, null, 2);
+
+    if (!map) {
+      map = L.map('map').setView([weatherData.coord.lat, weatherData.coord.lon], 6);
+    } else {
+      map.setView([weatherData.coord.lat, weatherData.coord.lon], 6);
+      map.eachLayer((layer) => {
+        if (layer instanceof L.Marker) {
+          map.removeLayer(layer);
+        }
+      });
     }
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19
+    }).addTo(map);
+    L.marker([weatherData.coord.lat, weatherData.coord.lon]).addTo(map);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-document.getElementById('getWeatherBtn').addEventListener('click', weather());
+document.getElementById('getWeatherBtn').addEventListener('click', weather);
 document.getElementById('cityInput').addEventListener('keypress', function (e) {
   if (e.key === 'Enter') {
     weather();
-  }
+  } 
 });
